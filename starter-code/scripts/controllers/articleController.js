7a96b5fb6648e6307af3370316674aac733d7fab
Clarefollett <chlaw101@gmail.com>
2017-01-02 14:13:43 -0800

    finished the last two todos which were deleting the handleMainNav function and deleted the call for it

--- starter-code/scripts/controllers/articleController.js
+++ starter-code/scripts/controllers/articleController.js
@@ -2,7 +2,7 @@
   var articleController = {};
 
   articleController.reveal = function() {
-    /* TODO: Reveal only the articles section! */
+    /* TODO: done! Reveal only the articles section! */
     //hide the about section
     $('section[id="about"]').hide();
     //reveal the article section
--- starter-code/scripts/controllers/routes.js
+++ starter-code/scripts/controllers/routes.js
@@ -1,4 +1,4 @@
-/* TODO: 1/2 done Configure our two routes for this app with page.js,
+/* TODO: done Configure our two routes for this app with page.js,
     by registering each URL your app can handle, linked to a
     single controller function to handle it: */
 page('/about', aboutController.reveal);
