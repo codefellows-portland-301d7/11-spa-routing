2478675cad78c1f35adbb4dab0ee26e2d0a078fb
Clarefollett <chlaw101@gmail.com>
2017-01-02 14:10:43 -0800

    we wrote the function for the articleController and we configured this function in the route.js page

--- starter-code/scripts/controllers/articleController.js
+++ starter-code/scripts/controllers/articleController.js
@@ -3,6 +3,10 @@
 
   articleController.reveal = function() {
     /* TODO: Reveal only the articles section! */
+    //hide the about section
+    $('section[id="about"]').hide();
+    //reveal the article section
+    $('section[id="articles"]').show();
   };
 
   module.articleController = articleController;
--- starter-code/scripts/controllers/routes.js
+++ starter-code/scripts/controllers/routes.js
@@ -2,6 +2,7 @@
     by registering each URL your app can handle, linked to a
     single controller function to handle it: */
 page('/about', aboutController.reveal);
+page('/', articleController.reveal);
 
 /* TODO: --DONE!-- What function do we call to activate page.js?
     Fire it off now, to execute it: */
