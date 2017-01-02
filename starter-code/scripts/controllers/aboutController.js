e81ed00b1214101fdf8fe117fdfcb0750495db02
cmd-kvn <wong.kevin@student.clackamas.edu>
2017-01-02 13:39:11 -0800

    testing functionality of aboutController and route call in routes.js

--- starter-code/scripts/controllers/aboutController.js
+++ starter-code/scripts/controllers/aboutController.js
@@ -3,6 +3,10 @@
 
   aboutController.reveal = function() {
     /* TODO: Reveal only the about section! */
+    // hide the article section in html
+    $('section[id="articles"]').hide();
+    // show the about section in html
+    $('section[id="about"]').show();
   };
 
   module.aboutController = aboutController;
