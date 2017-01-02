12352604d94b17393c62bf409cabb91d82cf4970
cmd-kvn <wong.kevin@student.clackamas.edu>
2017-01-02 13:47:08 -0800

    restored aboutController and routes after testing

--- starter-code/scripts/controllers/aboutController.js
+++ starter-code/scripts/controllers/aboutController.js
@@ -2,7 +2,7 @@
   var aboutController = {};
 
   aboutController.reveal = function() {
-    /* TODO: Reveal only the about section! */
+    /* TODO: done Reveal only the about section! */
     // hide the article section in html
     $('section[id="articles"]').hide();
     // show the about section in html
--- starter-code/scripts/controllers/routes.js
+++ starter-code/scripts/controllers/routes.js
@@ -1,7 +1,7 @@
-/* TODO: Configure our two routes for this app with page.js,
+/* TODO: 1/2 done Configure our two routes for this app with page.js,
     by registering each URL your app can handle, linked to a
     single controller function to handle it: */
-page('/testDone', aboutController.reveal);
+page('/about', aboutController.reveal);
 
 /* TODO: --DONE!-- What function do we call to activate page.js?
     Fire it off now, to execute it: */
